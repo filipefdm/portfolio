0db1d56af34c7f05b1783328acacdf959f6b5935
filipefdm <femotta96@gmail.com>
2022-09-29 14:32:47 -0300

    :sparkles: feat: create home hero component

---
+++ src/components/HomeHero/index.tsx
@@ -0,0 +1,40 @@
+/* eslint-disable react/jsx-no-comment-textnodes */
+import { Container, TextContainer, InfosContainer, CodeItem } from './styles';
+import picture from '../../assets/pessoa.webp';
+
+export default function HomeHero() {
+  return (
+    <Container>
+      <img src={picture} alt="Minha foto" />
+      <div>
+        <TextContainer>
+          <h1>Olá</h1>
+          <h2>Me chamo Filipe</h2>
+        </TextContainer>
+        <InfosContainer>
+          <CodeItem>
+            <span className="comment">// Minha apresentação</span>
+            <span className="purple">Infos</span> {'\u007B'}
+            <div>
+              Nome: <span className="blue">Filipe,</span>
+            </div>
+            <div>
+              Sobrenome: <span className="blue">Motta,</span>
+            </div>
+            {'\u007D'}
+          </CodeItem>
+          <CodeItem>
+            <span className="purple">Cargo</span> {'\u007B'}
+            <div>
+              Função: <span className="blue">Desenvolvedor Front-end,</span>
+            </div>
+            <div>
+              Empresa: <span className="blue">Grupo Ser Educacional,</span>
+            </div>
+            {'\u007D'}
+          </CodeItem>
+        </InfosContainer>
+      </div>
+    </Container>
+  );
+}
