b005b9f8d582c629a14d592d098e8b1f90485037
filipefdm <femotta96@gmail.com>
2022-10-03 12:01:05 -0300

    :recycle: refactor: add types, add loading screen component to fallback router, make banner project dynamic for prismic, create get static paths & props

--- src/pages/projects/[slug]/index.tsx
+++ src/pages/projects/[slug]/index.tsx
@@ -1,27 +1,86 @@
+import Prismic from '@prismicio/client';
+import { GetStaticPaths, GetStaticProps } from 'next';
+import { useRouter } from 'next/router';
 import { BannerProject } from '../../../components/BannerProject';
 import Header from '../../../components/Header';
+import { LoadingScreen } from '../../../components/LoadingScreen';
+import { getPrismicClient } from '../../../services/prismic';
 import { ProjectContainer } from '../../../styles/ProjectStyles';
 
-export default function Project() {
+interface IProject {
+  slug: string;
+  title: string;
+  type: string;
+  description: string;
+  link: string;
+  thumbnail: string;
+}
+
+interface ProjectProps {
+  project: IProject;
+}
+
+export default function Project({ project }: ProjectProps) {
+  const router = useRouter();
+
+  if (router.isFallback) {
+    return <LoadingScreen />;
+  }
+
   return (
     <ProjectContainer>
       <Header />
       <BannerProject
-        title="Projeto 01"
-        type="Website"
-        imgUrl="https://images.prismic.io/aulaportfolio/4062bd0a-5769-4dca-9b86-283af4c99654_isometric-psd-web-mockup-1-208.jpg?auto=compress,format"
+        title={project.title}
+        type={project.type}
+        imgUrl={project.thumbnail}
       />
 
       <main>
-        <p>
-          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Vero eum
-          laborum eos eligendi maiores maxime perspiciatis. Sapiente,
-          necessitatibus!
-        </p>
+        <p>{project.description}</p>
         <button type="button">
-          <a href="#">Ver projeto online</a>
+          <a href={project.link}>Ver o projeto online</a>
         </button>
       </main>
     </ProjectContainer>
   );
 }
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  const prismic = getPrismicClient();
+  const projects = await prismic.query([
+    Prismic.predicates.at('document.type', 'projeto')
+  ]);
+
+  const paths = projects.results.map(project => ({
+    params: {
+      slug: project.uid
+    }
+  }));
+
+  return {
+    paths,
+    fallback: true
+  };
+};
+
+export const getStaticProps: GetStaticProps = async context => {
+  const prismic = getPrismicClient();
+  const { slug } = context.params;
+
+  const response = await prismic.getByUID('projeto', String(slug), {});
+
+  const project = {
+    slug: response.uid,
+    title: response.data.title,
+    type: response.type,
+    description: response.data.description,
+    link: response.data.link.url,
+    thumbnail: response.data.thumbnail.url
+  };
+
+  return {
+    props: { project },
+    revalidate: 86400
+  };
+};
