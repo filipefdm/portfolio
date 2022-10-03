fd47764b3e819f67d78be26887ba4b6d7312605d
filipefdm <femotta96@gmail.com>
2022-10-03 12:02:32 -0300

    :recycle: refactor: add types, make projects dynamic, create get static props

--- src/pages/projects/index.tsx
+++ src/pages/projects/index.tsx
@@ -1,37 +1,66 @@
+import { GetStaticProps } from 'next';
+
+import Prismic from '@prismicio/client';
+import { getPrismicClient } from '../../services/prismic';
+
 import Header from '../../components/Header';
 import { ProjectItem } from '../../components/ProjectItem';
+
 import { ProjectsContainer } from '../../styles/ProjectsStyles';
 
-export default function Projects() {
+interface IProject {
+  slug: string;
+  title: string;
+  type: string;
+  description: string;
+  link: string;
+  thumbnail: string;
+}
+
+interface ProjectProps {
+  projects: IProject[];
+}
+
+export default function Projects({ projects }: ProjectProps) {
   return (
     <ProjectsContainer>
       <Header />
       <main className="container">
-        <ProjectItem
-          title="Projeto 01"
-          type="Website"
-          slug="teste"
-          imgUrl="https://images.prismic.io/aulaportfolio/4062bd0a-5769-4dca-9b86-283af4c99654_isometric-psd-web-mockup-1-208.jpg?auto=compress,format"
-        />
-        <ProjectItem
-          title="Projeto 02"
-          type="Website"
-          slug="teste"
-          imgUrl="https://images.prismic.io/aulaportfolio/4062bd0a-5769-4dca-9b86-283af4c99654_isometric-psd-web-mockup-1-208.jpg?auto=compress,format"
-        />
-        <ProjectItem
-          title="Projeto 03"
-          type="Website"
-          slug="teste"
-          imgUrl="https://images.prismic.io/aulaportfolio/4062bd0a-5769-4dca-9b86-283af4c99654_isometric-psd-web-mockup-1-208.jpg?auto=compress,format"
-        />
-        <ProjectItem
-          title="Projeto 04"
-          type="Website"
-          slug="teste"
-          imgUrl="https://images.prismic.io/aulaportfolio/4062bd0a-5769-4dca-9b86-283af4c99654_isometric-psd-web-mockup-1-208.jpg?auto=compress,format"
-        />
+        {projects.map(project => (
+          <ProjectItem
+            key={project.slug}
+            title={project.title}
+            type={project.type}
+            slug={project.slug}
+            imgUrl={project.thumbnail}
+          />
+        ))}
       </main>
     </ProjectsContainer>
   );
 }
+
+export const getStaticProps: GetStaticProps = async () => {
+  const prismic = getPrismicClient();
+
+  const projectResponse = await prismic.query(
+    [Prismic.Predicates.at('document.type', 'projeto')],
+    { orderings: '[document.first_publication_date desc]' }
+  );
+
+  const projects = projectResponse.results.map(project => ({
+    slug: project.uid,
+    title: project.data.title,
+    type: project.data.type,
+    description: project.data.description,
+    link: project.data.link,
+    thumbnail: project.data.thumbnail.url
+  }));
+
+  return {
+    props: {
+      projects
+    },
+    revalidate: 86400
+  };
+};
