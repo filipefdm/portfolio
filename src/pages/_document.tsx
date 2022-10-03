338f008b27f02cde860cb170c559ffa2a3e1ce83
filipefdm <femotta96@gmail.com>
2022-10-03 16:32:16 -0300

    :bug: fix: fix link preconnectors

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -42,7 +42,7 @@ export default class MyDocument extends Document {
       <Html lang="pt-br">
         <Head>
           <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link
             href="https://fonts.googleapis.com/css2?family=JetBrains+Mono:wght@200&family=Montserrat:wght@300;400;500;600;700&display=swap"
             rel="stylesheet"
