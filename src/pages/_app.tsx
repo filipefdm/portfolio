d5c1594a0ba3a62095cd89f69c1f11f60c0ceb1e
filipefdm <femotta96@gmail.com>
2022-10-03 16:31:11 -0300

    :recycle: refactor: add nextjs-progressbar & toaster to app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,7 @@
 import { ThemeProvider } from 'styled-components';
+import NextNProgress from 'nextjs-progressbar';
+
+import { Toaster } from 'react-hot-toast';
 
 import GlobalStyles from '../styles/global';
 import theme from '../styles/theme';
@@ -6,6 +9,15 @@ import theme from '../styles/theme';
 function MyApp({ Component, pageProps }) {
   return (
     <ThemeProvider theme={theme}>
+      <NextNProgress
+        color={theme.primary}
+        startPosition={0.3}
+        stopDelayMs={200}
+        height={3}
+        showOnShallow
+      />
+
+      <Toaster position="bottom-right" />
       <Component {...pageProps} />
       <GlobalStyles />
     </ThemeProvider>
